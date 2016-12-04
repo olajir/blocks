f910f124af9eeae9f7f10bfc4c5a879d71493a24
Ola Jirlow <ola@ohjays.com>
2016-12-04 22:09:18 +0100

    Corrected blimage.py

--- blimg/blimage.py
+++ blimg/blimage.py
@@ -1,7 +1,7 @@
 import imgbase
 
 bdef = imgbase.layout.BlockDef(36,38,20,20,7)
-adef = imgbase.layout.ArrayDef(7,2,900,(50,50),bdef)
+adef = imgbase.layout.ArrayDef(7,2,900,900,(50,50),bdef)
 
 
 def find_blocks(image, adef=adef):
